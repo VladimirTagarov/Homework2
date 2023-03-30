e8e55878af5b061d144b4cbc5256532b2e462a65
VladimirTagarov <tagarovl@mail.ru>
2023-03-30 14:16:12 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+let a = 10;
+alert(a);
+
+a = 20;
+alert(a);
+
+let languageCreator = 'Брендан Эйх';
+alert(languageCreator);
+
+let num_1 = 10;
+let num_2 = 2;
+alert(num_1 + num_2);
+alert(num_1 - num_2);
+alert(num_1 * num_2);
+alert(num_1 / num_2);
+
+let a = 9;
+let b = 2;
+alert(a % b);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num ++;
+num --;
+alert(num);
+
+let age = prompt('Сколько Вам лет?');
+alert(age);
+
+let user = {
+        name: 'Vladimir',
+        age: 32,
+        isAdmin: false
+    };
+user.cityOfResidence = 'Moscow';
+user.age = 33;
+delete user.cityOfResidence;
+let info = prompt('Какую информацию хотите узнать о пользователе?', 'name');
+alert(user[info]);
+
+let yourName = prompt('Введите ваше имя');
+alert(`Привет, ${yourName}!`);
